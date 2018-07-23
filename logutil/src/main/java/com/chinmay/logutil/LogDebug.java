5211b2396a224bf615ae22fbd84138b7b7bbba29
Chinmay Dighe <chinmay@Chinmays-MacBook-Pro.local>
2018-07-23 11:19:19 +0530

    Created a new project and a library to Debug

---
+++ logutil/src/main/java/com/chinmay/logutil/LogDebug.java
@@ -0,0 +1,22 @@
+package com.chinmay.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by chinmay on 7/23/18.
+ */
+
+public class LogDebug {
+
+
+
+    private static final String TAG = "SUPER_AWESOME_TAG";
+
+
+    public static void main d(String message){
+
+        Log.d(TAG,message);
+
+
+    }
+}
